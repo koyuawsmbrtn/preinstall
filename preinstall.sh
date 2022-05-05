a7bce4d6221a34ce591c08957069f58d7e998818
koyu <me@koyu.space>
2022-05-05 09:51:27 +0200

    Install ffmpeg

--- preinstall.sh
+++ preinstall.sh
@@ -64,7 +64,7 @@ sudo apt install -fy
 rm /tmp/appimagelauncher.deb
 
 # Software found in repositories
-sudo apt install --install-recommends openjdk-8-jre pavucontrol cpu-x synaptic gimp inkscape vlc weechat git gparted curl ubuntu-restricted-extras mlocate flatpak transmission-gtk p7zip libreoffice-style-breeze fonts-firacode hugo -y
+sudo apt install --install-recommends openjdk-8-jre pavucontrol cpu-x synaptic gimp inkscape vlc weechat git gparted curl ubuntu-restricted-extras mlocate flatpak transmission-gtk p7zip libreoffice-style-breeze fonts-firacode hugo ffmpeg -y
 
 # Amfora
 sudo wget -O /usr/bin/amfora -c "https://github.com/makeworld-the-better-one/amfora/releases/download/v1.9.2/amfora_1.9.2_linux_64-bit"
