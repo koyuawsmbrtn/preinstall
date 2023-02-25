046e503d67ee8bc062b0011540db07c5f0ccf76a
koyu <me@koyu.space>
2023-02-25 10:50:30 +0100

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -88,7 +88,7 @@ sudo apt install element-desktop -y
 sudo snap refresh
 sleep 3
 sudo snap refresh
-sudo snap install bitwarden spotify telegram-desktop
+sudo snap install bitwarden spotify telegram-desktop joplin-desktop
 
 # Autostart
 cp -r autostart ~/.config
