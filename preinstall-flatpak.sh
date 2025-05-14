5f3272f6ecd1ea4d02317d1985a087f7ed195c96
koyu <me@koyu.space>
2025-05-14 15:04:17 +0200

    Refactor Flatpak installation command for better readability

--- preinstall-flatpak.sh
+++ preinstall-flatpak.sh
@@ -30,7 +30,28 @@ sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flat
 flatpak update -y
 
 # Install applications via Flatpak
-flatpak install --system flathub com.bitwarden.desktop com.spotify.Client org.telegram.desktop md.obsidian.Obsidian com.visualstudio.code io.github.equicord.equibop org.gimp.GIMP org.inkscape.Inkscape org.videolan.VLC org.libreoffice.LibreOffice org.mozilla.firefox com.github.tchx84.Flatseal it.mijorus.gearlever org.telegram.desktop org.localsend.localsend_app de.schmidhuberj.DieBahn org.signal.Signal org.nickvision.tubeconverter io.github.thetumultuousunicornofdarkness.cpu-x app.zen_browser.zen -y
+flatpak install --system flathub \
+  com.bitwarden.desktop \
+  com.spotify.Client \
+  org.telegram.desktop \
+  md.obsidian.Obsidian \
+  io.github.equicord.equibop \
+  org.gimp.GIMP \
+  org.inkscape.Inkscape \
+  org.videolan.VLC \
+  org.libreoffice.LibreOffice \
+  com.github.tchx84.Flatseal \
+  it.mijorus.gearlever \
+  org.localsend.localsend_app \
+  de.schmidhuberj.DieBahn \
+  org.signal.Signal \
+  org.nickvision.tubeconverter \
+  io.github.thetumultuousunicornofdarkness.cpu-x \
+  app.zen_browser.zen \
+  com.valvesoftware.Steam \
+  com.heroicgameslauncher.hgl \
+  io.gitlab.adhami3310.Impression \
+  de.haeckerfelix.Fragments -y
 
 # Printing post-install steps
 echo -e
